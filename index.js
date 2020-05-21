bf76e4876701bbd142107342a9d3b547bbec91d3
Jesam <61584105+theofficialBF@users.noreply.github.com>
2020-05-21 19:43:18 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+
+function clickCounter() {
+  if (typeof(Storage) !== "undefined") {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount) + 1;
+    } else {
+      localStorage.clickcount = 1;
+    }
+    document.getElementById("button").innerHTML = "Total 'Your Mads' : " + localStorage.clickcount;
+
+    var audio = new Audio('Your mad.mp3');
+    audio.play();
+
+
+  } else {
+    document.getElementById("button").innerHTML = "Sorry, your browser does not support web storage...";
+  }
+};
